dd20a2a73fc9b137362127155ac246c68ba1f103
Sneha <sneha.mahadik@morrisonsplc.co.uk>
2019-07-20 20:20:10 +0530

    ready to publish

---
+++ precache-manifest.760f21cf9965216b7d3856cba2d13271.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ca9da694bb4bdb22f40b31719472897",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9e7a2041d67d3aee9a64",
+    "url": "/static/css/main.c6b8979b.chunk.css"
+  },
+  {
+    "revision": "162af04bae2cd2949ad9",
+    "url": "/static/js/2.4ff1927d.chunk.js"
+  },
+  {
+    "revision": "9e7a2041d67d3aee9a64",
+    "url": "/static/js/main.68edddbf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "fd61a5d0480702bc74b2e930b87f1bc4",
+    "url": "/static/media/StockInfo.fd61a5d0.bin"
+  }
+]);
